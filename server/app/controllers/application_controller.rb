a02c7715b00c07e2693b45ca7d630c42614642bb
Dan Sowter <dan@netengine.com.au>
2013-12-20 12:30:13 +1000

    More setup

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
+  attr_reader :current_user
+
   respond_to :json
   protect_from_forgery with: :null_session
 
   private
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      @current_user = User.find_by(ddg_access_token: token)
+    end
+  end
+
   def render_serialized(resource, options = {})
     render options.merge(json: resource)
                   .reverse_merge(SerializerFactory.new(resource).to_h)
